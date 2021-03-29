216243ff50f1f89acb73caef75a76a5ab3de7d75
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-28 22:12:17 -0400

    a lot cleaner. Made repetitive methods in abstract class with generics to be inherited by subclasses

--- src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/25/18.
  */
@@ -12,4 +14,76 @@ public abstract class DuplicateDeleter<T> implements DuplicateDeleterInterface<T
 
     abstract public T[] removeDuplicates(int maxNumberOfDuplications);
     abstract public T[] removeDuplicatesExactly(int exactNumberOfDuplications);
+
+    public int newLength(int num) {
+        int counter = 0;
+        int newLength = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[j].equals(array[i])) {
+                    counter++;
+                }
+            }
+            if (counter < num) {
+                newLength++;
+            }
+            counter = 0;
+        }
+        return newLength;
+    }
+
+    public T[] makeNewArr(int newLength, int num) {
+        T[] output = Arrays.copyOf(array, newLength);
+        int counter = 0;
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[j].equals(array[i])) {
+                    counter++;
+                }
+            }
+            if (counter < num) {
+                output[index] = array[i];
+                index++;
+            }
+            counter = 0;
+        }
+        return output;
+    }
+
+    public Integer newLengthExact(int num) {
+        int counter = 0;
+        int newLength = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[j].equals(array[i])) {
+                    counter++;
+                }
+            }
+            if (counter != num) {
+                newLength++;
+            }
+            counter = 0;
+        }
+        return newLength;
+    }
+
+    public T[] makeNewExactArray(int newLength, int num) {
+        T[] output = Arrays.copyOf(array, newLength);
+        int counter = 0;
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[j].equals(array[i])) {
+                    counter++;
+                }
+            }
+            if (counter != num) {
+                output[index] = array[i];
+                index++;
+            }
+            counter = 0;
+        }
+        return output;
+    }
 }
